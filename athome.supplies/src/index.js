8f63651874cacd0c35907ead22ce8dcc2315b9f0
André Ananias <anddregarcia@gmail.com>
2025-05-01 20:38:02 -0300

    adding express files and server config

---
+++ athome.supplies/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('node:path')
+const router = require('./routes')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true}))
+
+app.use(router)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando em http://localhost:${PORT}/`))
